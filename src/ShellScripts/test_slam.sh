fea60a72d44baff1a7f10706b345673f0d20a7b2
Koki Mitsunami <k.mitsunami@gmail.com>
2018-06-04 01:30:53 +0000

    update test_slam.sh

--- src/ShellScripts/test_slam.sh
+++ src/ShellScripts/test_slam.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-x-terminal-emulator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$HOME/catkin_ws/src/World/MyWorld.world\"" &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$HOME/catkin_ws/src/World/MyWorld.world\"" &
 sleep 5
-x-terminal-emulator  -e  " roslaunch turtlebot_navigation gmapping_demo.launch" &
+xterm  -e  " roslaunch turtlebot_navigation gmapping_demo.launch" &
 sleep 5
-x-terminal-emulator  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-x-terminal-emulator  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" 
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" 
 
