32417df2c5b2c9a5d3275edb8757ce799f5f7bba
Koki Mitsunami <k.mitsunami@gmail.com>
2018-06-04 01:54:25 +0000

    add wall_follower

---
+++ src/ShellScripts/wall_follower.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$HOME/catkin_ws/src/World/MyWorld.world\"" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_navigation gmapping_demo.launch" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun wall_follower wall_follower" 
+
