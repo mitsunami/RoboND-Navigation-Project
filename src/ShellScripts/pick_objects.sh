bda949189e3a48e8214aedaed3e247839a6407b4
Koki Mitsunami <k.mitsunami@gmail.com>
2018-06-04 08:23:26 +0000

    add initial pick_objects

---
+++ src/ShellScripts/pick_objects.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=\"$HOME/catkin_ws/src/World/MyWorld.world\"" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_navigation amcl_demo.launch map_file:=\"$HOME/catkin_ws/src/World/MyWorld.yaml\"" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects" 
+
