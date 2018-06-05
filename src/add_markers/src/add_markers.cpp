48a81ab82dc14130d6c3f581e8b1174266528d4c
Koki Mitsunami <k.mitsunami@gmail.com>
2018-06-05 14:48:25 +0000

    confirmed home_service.sh

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -25,23 +25,12 @@ int main( int argc, char** argv )
   while (ros::ok())
   {
     visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
     marker.ns = "basic_shapes";
     marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
     marker.type = shape;
-
-    ROS_INFO("Publish the marker at the pickup zone.");
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
     marker.pose.position.x = 4.0;
     marker.pose.position.y = 2.0;
     marker.pose.position.z = 0;
@@ -49,26 +38,29 @@ int main( int argc, char** argv )
     marker.pose.orientation.y = 0.0;
     marker.pose.orientation.z = 0.0;
     marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 0.25;
     marker.scale.y = 0.25;
     marker.scale.z = 0.25;
-
-    // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
     marker.color.a = 1.0;
-
     marker.lifetime = ros::Duration();
 
     // Initially show the marker at the pickup zone
-    // Publish the marker
+    ROS_INFO("Publish the marker at the pickup zone.");
+    while (marker_pub.getNumSubscribers() < 1){
+      if (!ros::ok()){
+	return 0;
+      }
+      ROS_WARN_ONCE("Please create a subscriber to the marker");
+      sleep(1);
+    }
     marker_pub.publish(marker);
+    ros::Duration(1.0).sleep();
 
     double marker_dist = 100.0;
-    const double dist_threshold = 0.1;
+    const double dist_threshold = 0.3;
     while( marker_dist > dist_threshold ){
       marker_dist = sqrt( pow(odom_x-marker.pose.position.x, 2) + pow(odom_y-marker.pose.position.y, 2) );
       ros::spinOnce();
